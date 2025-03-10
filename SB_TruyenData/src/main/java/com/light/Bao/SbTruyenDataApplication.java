69b3531ea5d068c4d4dddcde9d982cf3799afe45
TranQuocBao-Mole <anhbaohero090@gmail.com>
2025-03-10 22:59:29 +0700

    SB_TruyenData
    push_SB_TransmitData

---
+++ SB_TruyenData/src/main/java/com/light/Bao/SbTruyenDataApplication.java
@@ -0,0 +1,13 @@
+package com.light.Bao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTruyenDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTruyenDataApplication.class, args);
+	}
+
+}
---
+++ SB_TruyenData/src/test/java/com/light/Bao/SbTruyenDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.light.Bao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTruyenDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
