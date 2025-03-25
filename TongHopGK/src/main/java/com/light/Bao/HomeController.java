ef0b1558f7e1959350d56c3e3ccef209cc408109
TranQuocBao-Mole <anhbaohero090@gmail.com>
2025-03-25 10:13:57 +0700

    push

--- TongHopGK/src/main/java/com/light/Bao/HomeController.java
+++ TongHopGK/src/main/java/com/light/Bao/HomeController.java
@@ -1,5 +1,23 @@
 package com.light.Bao;
 
-public class HomeController {
+import org.springframework.web.bind.annotation.GetMapping;
 
+public class HomeController {
+	  @GetMapping("/")
+	    public String home() {
+	        return "frontEnd/index";
+	    }
+	  @GetMapping("/")
+	    public String list() {
+	        return "frontEnd/list";
+	    }
+	  @GetMapping("/")
+	    public String addnew() {
+	        return "frontEnd/addnew";
+	    }
+
+	    @GetMapping("/about")
+	    public String about() {
+	        return "frontEnd/about";
+	    }
 }
