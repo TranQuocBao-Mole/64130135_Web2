674cc2d87e89f2107bed5c800d5db46e35fc4976
TranQuocBao-Mole <anhbaohero090@gmail.com>
2025-04-01 10:11:48 +0700

    push

--- TranQuocBao_KTgiuaKi/src/main/java/thiGK/ntu64130135/Controller/HomeController.java
+++ TranQuocBao_KTgiuaKi/src/main/java/thiGK/ntu64130135/Controller/HomeController.java
@@ -1,5 +1,4 @@
 package thiGK.ntu64130135.Controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
